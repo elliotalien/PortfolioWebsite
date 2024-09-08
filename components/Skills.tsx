aa1db6169870ca73afdc34169c92b7649eac2132
elliotalien <blackdevilhacker4@gmail.com>
2024-09-08 16:16:36 +0530

    Skills Fix

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Skills = () => {
   return (
     <section>
-      <div className='text-4xl text-center'>Skils & Tools</div>
+      <div className='text-4xl text-center'>Skills & Tools</div>
       <div className="logos group relative overflow-hidden whitespace-nowrap py-10 [mask-image:_linear-gradient(to_right,_transparent_0,_white_128px,white_calc(100%-128px),_transparent_100%)]">
         <div className="animate-slide-left-infinite group-hover:animation-pause inline-block w-max">
           {imageData.map((image, index) => (
